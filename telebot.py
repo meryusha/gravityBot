6d2411462d7c8852d63266c7bd5847ba645d03bc
Merey <merey.ramazanova@nu.edu.kz>
2018-05-05 23:23:03 +0600

    commit 3 (useless)

--- telebot.py
+++ telebot.py
@@ -2,7 +2,7 @@ import telepot
 import sys
 import time
 from telepot.loop import MessageLoop
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 
 
 
@@ -10,11 +10,15 @@ from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='Press me', callback_data='press')],
-               ])
-
-    bot.sendMessage(chat_id, 'Use inline keyboard', reply_markup=keyboard)
+    
+    if content_type == 'text':
+        if msg['text'] == '/key':
+            bot.sendMessage(chat_id, 'testing custom keyboard',
+                            reply_markup=ReplyKeyboardMarkup(
+                                keyboard=[
+                                    [KeyboardButton(text="Ilyuha kakashka"), KeyboardButton(text="Ilyuha kozyavka")]
+                                ]
+                            ))
 
 def on_callback_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
@@ -26,7 +30,7 @@ def on_callback_query(msg):
 bot = telepot.Bot('512216973:AAH8Rn3ZrBwWDx-q23nhWbDYUJVlRcbYT2o')
 #print(bot.getMe())
 #bot = telepot.Bot(TOKEN)
-#MessageLoop(bot, {'chat': on_chat_message,'callback_query': on_callback_query}).run_as_thread()
+MessageLoop(bot, {'chat': on_chat_message,'callback_query': on_callback_query}).run_as_thread()
 print('Listening ...')
 
 while 1:
